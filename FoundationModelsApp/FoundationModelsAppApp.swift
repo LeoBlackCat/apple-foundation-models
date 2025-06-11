a45a18380a77ebebf8caf9ae868569198ba795ea
LeoBlackCat <leonid.ivanov@gmail.com>
2025-06-11 16:26:51 +0600

    Initial Commit

---
+++ FoundationModelsApp/FoundationModelsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationModelsAppApp.swift
+//  FoundationModelsApp
+//
+//  Created by Leo on 11/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct FoundationModelsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
